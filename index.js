3516d0bee9f43afc54a145532bb6d24d3ba76812
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-03-15 17:05:40 -0700

    Refactor and pseudo code plan

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const chalk = require('chalk');
+const prompts = require('prompts');
 const updateNotifier = require('update-notifier');
 
 const pkg = require('./package.json');
 const notifier = updateNotifier({ pkg });
 
+if (notifier.update) {
+  notifier.notify();
+}
 
-  if (notifier.update) {
-    notifier.notify();
-  }
+const git = require('./utils/git');
+
+git.buildListArray().then(result => {
+    console.log(result)
+});
+
+
+
+// Build branch list
+// Print branch list to prompt
+// branches should be present on local, but not on origin
+// change view to view prompts on origin vs local
+// Sade - pass flags for local or origin
 
